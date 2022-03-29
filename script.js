eba24a254a8560563b44ec001f1fa290d4383f69
richharv557 <richharv557@gmail.com>
2022-03-28 22:31:11 -0500

    Working draft - successfully completed game logic.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// First function is to have the computer pick a random number, and assign that to "rock" "paper" or "scissors"
+
 function computerPlay() {
    let number = Math.floor(Math.random() * 3 + 1);
  // Randomly select a number between 1 and 3. The + 1 is to move the range from 0-2 to 1-3. 
@@ -14,8 +16,10 @@ function computerPlay() {
    } return output;
 }
 
+//This function is to run a round of rock paper scissors. Contains the actual showdown and results in a victory, loss, or tie message.
+
 function playRound(playerSelection, computerSelection) {
-// Start round with a user prompt selection, and a random selection for the computer.
+// Start round with a user prompt selection (enforced lower case), and a random selection for the computer.
     playerSelection = prompt("Enter rock, paper, or scissors.").toLowerCase()
     computerSelection = computerPlay()
 // Start with tie to eliminate 3 inputs, then run through win/loss conditions for all three options.
@@ -36,17 +40,23 @@ function playRound(playerSelection, computerSelection) {
     }        
 }
 
+// This is the 5 round implementation of the series.
+
 function game() {
+// start both players at 0    
     let yourScore = 0
     let computerScore = 0
+// for loop to cycle through 5 rounds exactly
     for (let i = 0; i < 5; i++) {
-       roundResult = alert(playRound());
+// the return message from the playRound() function contains "win" or "lose", which indicates how which score variable to increment
+       let roundResult = playRound();
+       alert(roundResult);
        if (roundResult.includes("win")) {
             yourScore++;
        } else if (roundResult.includes("lose")) {
            computerScore++;
        } 
-       console.log(yourScore + " - " +computerScore) // your code here!
+       alert(yourScore + " - " +computerScore) // your code here!
      }
-    console.log("Game over");
+    alert(`Game over! The final score was: ${yourScore} - ${computerScore}`);
     }
